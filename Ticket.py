9fc92b589d684ba45e9e685199de4e587627713c
Kush Patel <94019918+kushpatel7764@users.noreply.github.com>
2024-05-05 17:50:42 -0400

    Added a Ticket.py file

---
+++ Ticket.py
@@ -0,0 +1,52 @@
+#  Created by Kush Patel on 2/29/24.
+
+
+class Ticket: # Extra information about a ticket from scanned code and make a ticket class.
+    #Class Variable
+    #Consdiering including variables that Setters set instead of doing it at Getters.
+    #Setters
+    def setGameNum(scannedCode):
+        pass
+    def setID(scannedCode):
+        pass
+    def setTicketNum(scannedCode):
+        pass
+    def setPrice(scannedCode):
+        pass
+    def setBookAmount(scannedCode):
+        pass
+    #Getters
+    def getGameNum():
+        pass
+    def getID():
+        pass
+    def getTicketNum():
+        pass
+    def getPrice():
+        pass
+    def getBookAmount():
+        pass
+    
+   
+
+
+class TicketStatus:
+    open = []
+    close = []
+    
+    def __init__():
+        open = []
+        close = []
+    
+    def appendToOpen():
+        pass
+    def appendToClose():
+        pass
+    
+    def setToOpenTickets():
+        pass
+    
+    def getOpenTickets():
+        pass
+    def getCloseTickets():
+        pass
